9b9e6552e109380f3aef39f73b19b46399294dcf
vincent gabriel maris dafre <vinvinvinvin123@gmail.com>
2018-08-01 21:17:11 -0300

    primeira janela

---
+++ senac-dd-20182-atividades-vincentGabrielMarisDafre/src/main/java/br/sc/senac/dd/App.java
@@ -0,0 +1,10 @@
+package br.sc.senac.dd;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       
+    }
+}
---
+++ senac-dd-20182-atividades-vincentGabrielMarisDafre/src/main/java/br/sc/senac/dd/gui/Menu.java
@@ -0,0 +1,12 @@
+package br.sc.senac.dd.gui;
+
+import javax.swing.JOptionPane;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showConfirmDialog(null, "primeira janela");
+	}
+
+}
