00575ee2c338fd8676bcdb98836d8231a68d55ec
vincent gabriel maris dafre <vinvinvinvin123@gmail.com>
2018-08-01 21:10:18 -0300

    refactor project

---
+++ senac-dd-20182-atividades-vincentGabrielMarisDafre/src/main/java/senac_dd_20182_atividades_vincentGabrielMarisDafre/senac_dd_20182_atividades_vincentGabrielMarisDafre/App.java
@@ -0,0 +1,13 @@
+package senac_dd_20182_atividades_vincentGabrielMarisDafre.senac_dd_20182_atividades_vincentGabrielMarisDafre;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
